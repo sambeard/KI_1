4e1276669b340d92dbd063c803142cdc84d0dbad
Glorie,L. (Luuk) <l.glorie@students.uu.nl>
2018-12-05 15:28:16 +0100

    Add dfs and bfs

--- Part1_Search/search.py
+++ Part1_Search/search.py
@@ -72,24 +72,11 @@ def tinyMazeSearch(problem):
     w = Directions.WEST
     return  [s, s, w, s, w, w, s, w]
 
-def depthFirstSearch(problem):
-    """
-    Search the deepest nodes in the search tree first.
-
-    Your search algorithm needs to return a list of actions that reaches the
-    goal. Make sure to implement a graph search algorithm.
-
-    To get started, you might want to try some of these simple commands to
-    understand the search problem that is being passed in:
-
-    print "Start:", problem.getStartState()
-    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
-    print "Start's successors:", problem.getSuccessors(problem.getStartState())
-    """
+def graphSearch(problem, fringe):
+    
     startState = problem.getStartState()
     visited = set()
     parents = dict()
-    fringe = util.Stack()
     fringe.push(startState)
 
     while not fringe.isEmpty():
@@ -109,10 +96,26 @@ def depthFirstSearch(problem):
 
     return []
 
+def depthFirstSearch(problem):
+    """
+    Search the deepest nodes in the search tree first.
+
+    Your search algorithm needs to return a list of actions that reaches the
+    goal. Make sure to implement a graph search algorithm.
+
+    To get started, you might want to try some of these simple commands to
+    understand the search problem that is being passed in:
+
+    print "Start:", problem.getStartState()
+    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
+    print "Start's successors:", problem.getSuccessors(problem.getStartState())
+    """
+    return graphSearch(problem, util.Stack())
+
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    
+    return graphSearch(problem, util.Queue())
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
