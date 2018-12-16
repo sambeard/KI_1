9bd2d2471bca03c202a13d69829110a9d4a7522b
Sam Beard <sam.beard@zelezny.nl>
2018-12-16 12:05:59 +0100

    add comments to sub questions of q3 analysis

--- Part3_Reinforcement/analysis.py
+++ Part3_Reinforcement/analysis.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -25,39 +25,39 @@ def question2():
     return answerDiscount, answerNoise
 
 def question3a():
+    # close exit, risk cliff
     answerDiscount = 0.9        # regular discount
     answerNoise = 0.1           # slightly lower noise
     answerLivingReward = -5.0   # high negativeliving reward
     return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
 
 def question3b():
+    # close exit, avoid cliff
     answerDiscount = 0.3        # high discount factor
     answerNoise = 0.2           # regular noise
     answerLivingReward = -0.5   # regular negative living reward
     return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
 
 def question3c():
+    # distant exit, risk cliff
     answerDiscount = 0.9        # regular discount factor
     answerNoise = 0.0           # no noise
     answerLivingReward = -2     # relatively high negative living reward
     return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
 
 def question3d():
+    # distant exit, avoid cliff
     answerDiscount = 0.9        # regular discount reward
     answerNoise = 0.2           # regular noise
     answerLivingReward = 0      # no living reward
     return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
 
 def question3e():
+    # avoid all terminal states
     answerDiscount = 0.9        # regular discount reward
     answerNoise = 0.2           # regular noise
     answerLivingReward = 1      # positive living reward
     return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
 
 def question6():
     answerEpsilon = None
